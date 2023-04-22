a84f44c2b2af99dca31058b6512978d55c81f145
Benefrancis <benefrancis@gmail.com>
2023-04-22 18:08:51 -0300

    Implementei a busca utilizando API

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -9,7 +9,7 @@ import java.net.http.HttpResponse;
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(URI.create("https://www.omdbapi.com/?t=top+gun&apiKey=8080bbc8")).build();
+        HttpRequest request = HttpRequest.newBuilder(URI.create("https://www.omdbapi.com/?t=8mm&apiKey=8080bbc8")).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
     }
